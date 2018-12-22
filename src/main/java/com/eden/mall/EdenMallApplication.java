8ad045a785d2d5ab248c084bf3ffcd7b39a09438
nycqw <295838198@qq.com>
2018-12-22 19:26:37 +0800

    modify

--- src/main/java/com/eden/mall/EdenMallApplication.java
+++ src/main/java/com/eden/mall/EdenMallApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDubbo
 @MapperScan("com.eden.mall.mapper")
 @EnableAsync
-@EnableScheduling
 public class EdenMallApplication {
 
     public static void main(String[] args) {
